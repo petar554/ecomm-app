7e317acc5adf692765a7df148200c28c3ad0cb61
petarPAN <petarburic554@gmail.com>
2021-03-16 20:21:15 +0100

    Dispalying HTML (sign up form)

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const app = express();
 // req is object taht represent incoming request from the browser into web server
 // res represents outgoing response form server back to the browser
 app.get('/', (req, res) => {
-    res.send('web dev');
+    res.send(`
+        <div>
+            <form>
+                <input placeholder='email'/>
+                <input placeholder='password'/>
+                <input placeholder='password confirmation'/>
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `)
 });
 
 app.listen(3000, () => {
