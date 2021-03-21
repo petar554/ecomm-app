3b3a1680d2382a31da1a6137c00d13c62a14f710
petarPAN <petarburic554@gmail.com>
2021-03-21 10:44:36 +0100

    Add middleware

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
-// req is object taht represent incoming request from the browser into web server
-// res represents outgoing response form server back to the browser
+// middleware - is going automatically detect whether or not we are using get or post request (not apply int he case of a get request!)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// req object represents incoming request from the browser into web server
+// res represents outgoing response from server back to the browser
 app.get('/', (req, res) => {
     res.send(`
         <div>
@@ -13,21 +18,31 @@ app.get('/', (req, res) => {
                 <button>Sign Up</button>
             </form>
         </div>
-    `)
+    `);
 });
+s
+// middleware
+/* const bodyParser = (req, res, next) => {
+    if (req.method === POST) {
+        req.on('data', data => {
+            const parsed = data.toString('utf8').split('&');
+            const formData = {};
+            for (let pair of parsed) {
+                const [key, value] = pair.split('=');
+                formData[key] = value;
+            }
+            req.body = formData;
+            next();
+        });
+    } else {
+        next();
+    }
+} */
 
 app.post('/', (req, res) => {
-    req.on('data', data => {
-        const parsed = data.toString('utf8').split('&');
-        const formData = {};
-        for (let pair of parsed) {
-            const [key, value] = pair.split('=');
-            formData[key] = value;
-        }
-        console.log(formData);
-    });
-    res.send('acc created')
-})
+    console.log(req.body)
+    res.send('acc created');
+});
 
 app.listen(3000, () => {
     console.log('listening')
